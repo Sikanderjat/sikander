80dda4c57083c48b97dc30c40e109daef987bf41
Sikander <85985761+Sikanderjat@users.noreply.github.com>
2026-03-09 17:59:59 +0530

    Add sitemap route to serve sitemap.xml

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
 from flask import Flask, render_template
+from flask import send_from_directory
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return render_template('index.html')
+@app.route('/sitemap.xml')
+def sitemap():
+    return send_from_directory(app.static_folder, 'sitemap.xml')
